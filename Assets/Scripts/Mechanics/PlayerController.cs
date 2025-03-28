f653cdf68603539252a9535fe884fe244b507dfb
waxhot <uberductdude@gmail.com>
2025-03-28 15:35:23 -0700

    Update PlayerController.cs

--- Assets/Scripts/Mechanics/PlayerController.cs
+++ Assets/Scripts/Mechanics/PlayerController.cs
@@ -22,7 +22,7 @@ namespace Platformer.Mechanics
         /// <summary>
         /// Max horizontal speed of the player.
         /// </summary>
-        public float maxSpeed = 7;
+        public float maxSpeed = 30;
         /// <summary>
         /// Initial jump velocity at the start of a jump.
         /// </summary>
